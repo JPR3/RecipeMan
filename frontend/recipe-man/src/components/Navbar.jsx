724dfc5f7558499b55ad93a8f0d1f80f070b0af1
JPR3 <jpr3@riganati.com>
2025-07-02 15:19:07 -0400

    Added basic registration form, doesn't do anything with the data yet

---
+++ frontend/recipe-man/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="w-full bg-gray-900 border-b border-gray-600 text-white px-6 py-4 flex justify-between items-center fixed top-0 z-50 shadow-md">
+            <Link to="/" className="text-lg font-semibold hover:underline">
+                Home
+            </Link>
+            <Link to="/profile" className="text-lg font-semibold hover:underline">
+                Profile
+            </Link>
+        </nav>
+    );
+};
+
+export default Navbar;
