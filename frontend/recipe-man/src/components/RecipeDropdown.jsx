184b5e07f311701b4519fc01a4ab4289906c68ce
JPR3 <jpr3@riganati.com>
2025-07-16 15:21:21 -0400

    Changed class to className, fixed typos

--- frontend/recipe-man/src/components/RecipeDropdown.jsx
+++ frontend/recipe-man/src/components/RecipeDropdown.jsx
@@ -39,9 +39,9 @@ const RecipeDropdown = ({ recipeName, recipeTags, recipeId }) => {
             </div>
         )}
         <div className="ml-auto flex-shrink-0 content-center">
-            {isOpen ? (<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="var(--color-primary)" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+            {isOpen ? (<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="var(--color-primary)" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
                 <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-            </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="var(--color-primary)" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="var(--color-primary)" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>)}
         </div>
