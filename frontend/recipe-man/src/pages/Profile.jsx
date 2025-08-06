1fad43a5dc7cd520312e72217e99fb18c7d8562f
JPR3 <jpr3@riganati.com>
2025-08-06 17:52:19 -0400

    Added search functionality to profile page, updated ssearch funcitonality on recipes page with clear button

--- frontend/recipe-man/src/pages/Profile.jsx
+++ frontend/recipe-man/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ const Profile = () => {
     const [editModal, setEditModal] = useState(false);
     const [editElement, setEditElement] = useState(null);
     const [deleteElement, setDeleteElement] = useState(null);
+    const [searchValue, setSearchValue] = useState('');
     const { session, user, loading } = useAuth();
     if (loading) {
         return <div className="text-content p-4">Loading...</div>;
@@ -25,7 +26,7 @@ const Profile = () => {
     const uid = user?.id;
 
     React.useEffect(() => {
-        if (!deleteModal) {
+        if (!deleteModal && !newModal && !editModal) {
             fetch(`http://localhost:3000/api/users/${uid}`, {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
@@ -33,7 +34,7 @@ const Profile = () => {
             }).then(response => response.json()).then(data => {
                 setUsername(data.username);
             });
-            fetch(`http://localhost:3000/api/users/${uid}/tags/custom`, {
+            fetch(`http://localhost:3000/api/users/${uid}/tags/custom?` + new URLSearchParams({ name: searchValue.toLowerCase().trim() }).toString(), {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
@@ -41,7 +42,7 @@ const Profile = () => {
                 setTags(data);
             });
 
-            fetch(`http://localhost:3000/api/users/${uid}/ingredients/custom`, {
+            fetch(`http://localhost:3000/api/users/${uid}/ingredients/custom?` + new URLSearchParams({ name: searchValue.toLowerCase().trim() }).toString(), {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
@@ -49,7 +50,7 @@ const Profile = () => {
                 setIngredients(data);
             });
 
-            fetch(`http://localhost:3000/api/users/${uid}/units/custom`, {
+            fetch(`http://localhost:3000/api/users/${uid}/units/custom?` + new URLSearchParams({ name: searchValue.toLowerCase().trim() }).toString(), {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
@@ -58,7 +59,7 @@ const Profile = () => {
             });
         }
 
-    }, [accessToken, deleteModal, newModal, editModal, tab]);
+    }, [accessToken, deleteModal, newModal, editModal, searchValue, tab]);
 
     const handleSignOut = async () => {
         const { error } = await supabase.auth.signOut();
@@ -66,10 +67,25 @@ const Profile = () => {
 
     return (
         <div className="flex flex-col justify-start items-center w-full">
-            <div className="grid gap-9 justify-center border border-border shadow-lg bg-surface p-6 rounded-md max-w-md">
+            <div className="grid gap-9 justify-center border border-border shadow-lg bg-surface p-6 mb-4 rounded-md max-w-md">
                 <h1 className="text-2xl w-full">Profile</h1>
                 <p className="text-content">This is your profile page, {username}</p>
             </div>
+            <div className="flex flex-h content-center items-center rounded-md border border-border focus-within:outline-2 focus-within:outline-primary px-1 bg-fields text-content h-6.5 max-w-1/8">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="w-full text-content"
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                />
+                {searchValue && searchValue.length > 0 &&
+                    <svg onClick={() => setSearchValue("")} xmlns="http://www.w3.org/2000/svg" width="25" height="25" className="bi bi-x fill-content hover:fill-red-700 cursor-pointer" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                    </svg>
+                }
+            </div>
+
             <div className="w-full flex justify-center items-end max-w-1/2">
                 <div className="border-b-2 border-border w-full"></div>
                 <h2
--- frontend/recipe-man/src/pages/Recipes.jsx
+++ frontend/recipe-man/src/pages/Recipes.jsx
@@ -88,13 +88,20 @@ const Recipes = () => {
                         ))}
                     </div>
                 </div>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="rounded-md border border-border focus:border-2 px-1 bg-fields text-content h-6.5 w-full max-w-3/8"
-                    value={searchValue}
-                    onChange={(e) => setSearchValue(e.target.value)}
-                />
+                <div className="flex flex-h content-center items-center rounded-md border border-border focus-within:outline-2 focus-within:outline-primary px-1 bg-fields text-content h-6.5 max-w-3/8">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="w-full text-content"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                    {searchValue && searchValue.length > 0 &&
+                        <svg onClick={() => setSearchValue("")} xmlns="http://www.w3.org/2000/svg" width="25" height="25" className="bi bi-x fill-content hover:fill-red-700 cursor-pointer" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                        </svg>
+                    }
+                </div>
             </div>
             <button className="cursor-pointer border border-border rounded-2xl bg-primary hover:bg-primary-hv px-2" onClick={() => setRecipeModal(true)}>
                 New+
