af2741290b4b8e5845a8a5a2cf2cb73f3f420bd9
JPR3 <jpr3@riganati.com>
2025-08-19 16:27:20 -0400

    Added funcitonality to delete lists

---
+++ frontend/recipe-man/src/components/DeleteListModal.jsx
@@ -0,0 +1,47 @@
+import { useAuth } from "../AuthProvider";
+import Modal from "../components/Modal";
+
+const DeleteListModal = ({ openModal, closeModal, listId, listTitle }) => {
+    const { session, user, loading } = useAuth();
+    if (loading) return <div>Loading...</div>;
+    const accessToken = session?.access_token;
+    const uid = user?.id;
+
+    const handleDelete = () => {
+        fetch(`http://localhost:3000/api/users/${uid}/lists/${listId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            console.log("List deleted successfully");
+            closeModal();
+        }).catch(error => console.error("Error deleting list:", error));
+    }
+
+    return (
+        <Modal openModal={openModal} closeModal={closeModal}>
+            <div className="flex flex-col justify-center items-center gap-4">
+                <h2 className="text-lg font-semibold">Are you sure you want to delete "{listTitle}"?</h2>
+                <div className="flex gap-4 pb-2">
+                    <button
+                        className="bg-red-700 text-white px-4 py-2 rounded-full hover:bg-red-800 cursor-pointer"
+                        onClick={() => {
+                            handleDelete();
+                        }}
+                    >
+                        Delete
+                    </button>
+                    <button
+                        className="bg-button px-4 py-2 rounded-full hover:bg-border text-white cursor-pointer"
+                        onClick={closeModal}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+
+export default DeleteListModal;
--- frontend/recipe-man/src/pages/Lists.jsx
+++ frontend/recipe-man/src/pages/Lists.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import { useAuth } from "../AuthProvider";
 import { Link } from 'react-router-dom';
 import NewListModal from '../components/NewListModal';
+import DeleteListModal from '../components/DeleteListModal';
 
 const Lists = () => {
     const [lists, setLists] = useState([]);
     const [newModal, setNewModal] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
+    const [deleteModalData, setDeleteModalData] = useState({ id: "-1", title: "" })
     const { session, user, loading } = useAuth();
     if (loading) {
         return <div className="text-content p-4">Loading...</div>;
@@ -22,14 +25,24 @@ const Lists = () => {
         }).then(response => response.json()).then(data => {
             setLists(data);
         });
-    }, [accessToken]);
+    }, [accessToken, deleteModal]);
 
+    const handleDelete = (list) => {
+        setDeleteModalData({ id: list.id, title: list.title })
+        setDeleteModal(true)
+    }
     return (
         <div className="flex flex-col justify-start items-center w-full px-16 gap-2">
             <NewListModal
                 openModal={newModal}
                 closeModal={() => setNewModal(false)}
                 existingLists={lists.map(l => l.title)}
+            />
+            <DeleteListModal
+                openModal={deleteModal}
+                closeModal={() => { setDeleteModal(false); setDeleteModalData({ id: "-1", title: "" }) }}
+                listId={deleteModalData.id}
+                listTitle={deleteModalData.title}
             />
             <h1 className="text-5xl font-semibold text-content mb-2">Lists</h1>
             <button
@@ -41,10 +54,13 @@ const Lists = () => {
                 <div className="flex flex-col w-full gap-2 overflow-auto max-h-dvh">
                     {
                         lists.map((list, index) => (
-                            <div key={index} className="border-b-2 w-full border-border flex pb-2">
+                            <div key={index} className="border-b-2 w-full border-border flex pb-2 items-center">
                                 <Link to={`/lists/${list.id}`} className="w-full hover:underline">
                                     {list.title}
                                 </Link>
+                                <svg onClick={() => handleDelete(list)} xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-x fill-content hover:fill-red-700 cursor-pointer" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                                </svg>
                             </div>
                         ))
                     }
