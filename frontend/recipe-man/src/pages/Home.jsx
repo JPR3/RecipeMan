ae49c2aed2843ed5c2f3d18879cb14ac07cbeeb7
JPR3 <jpr3@riganati.com>
2025-08-26 13:20:46 -0400

    Added mobile formatting to home page

--- frontend/recipe-man/src/pages/Home.jsx
+++ frontend/recipe-man/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "../ThemeContext";
 import { useAuth } from "../AuthProvider";
 import { useNavigate } from "react-router-dom";
@@ -16,72 +16,139 @@ const Home = () => {
     const { theme } = useContext(ThemeContext);
     const { user } = useAuth();
     const navigate = useNavigate();
+    const [width, setWidth] = useState(0);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => setWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+            handleResize();
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, []);
     return (
-        <div className="flex flex-col w-full items-center">
-            <img src={logo} className="h-24" />
-            <h1 className="text-5xl font-semibold text-primary mb-4">RecipeMan</h1>
-            <div className="w-full flex justify-center mb-8">
-                <div className="flex border-border border-2 bg-surface text-content px-2 py-2 rounded-lg ">
-                    <p className="text-content text-xl content-center">Save and manage all your recipes and shopping lists in one place<br />
+        width > 768 ? (
+            <div className="flex flex-col w-full items-center">
+                <img src={logo} className="h-24" />
+                <h1 className="text-5xl font-semibold text-primary mb-4">RecipeMan</h1>
+                <div className="w-full flex justify-center mb-8">
+                    <div className="flex border-border border-2 bg-surface text-content px-2 py-2 rounded-lg ">
+                        <p className="text-content text-2xl content-center px-1 py-1">Save and manage all your recipes and shopping lists in one place<br />
+                            Seamlessly create shopping lists based on the ingredients you need for your recipes<br />
+                            Organize your ingredients and recipes with customizable tags<br />
+                            Create custom ingredients and measurement units to fit your needs</p>
+                    </div>
+                </div>
+                <div className="w-full flex justify-end mb-8">
+                    <div className="flex border-border border-t-2 border-b-2 border-l-2 bg-surface text-content px-2 py-2 rounded-l-lg max-w-11/12 gap-8">
+                        <img
+                            src={theme === "dark" ? recipes_dark : recipes_light}
+                            className="max-w-full max-h-full h-75 rounded-lg border-1 border-border"
+                        />
+                        <p className="text-content text-2xl content-center">Use the 'recipes' page to view, edit, and create new recipes<br />
+                            Search and filter recipes by tags to find exactly what you're looking for<br />
+                            Scale recipes to adjust ingredient quantities based on your needs<br />
+                            {"Tip: You can use {{double brackets}} around a number in the instructions of your recipe to enable scaling"}</p>
+
+                    </div>
+                </div>
+                <div className="w-full flex justify-start mb-8">
+                    <div className="flex border-border border-t-2 border-b-2 border-r-2 bg-surface text-content px-2 py-2 rounded-r-lg max-w-11/12 gap-8">
+                        <p className="text-content text-2xl content-center">Use the 'lists' page to make and manage your shopping lists<br />
+                            List items can be sorted by tags for easy organization<br />
+                            Multiple instances of the same item with compatible units will be automatically combined</p>
+                        <img
+                            src={theme === "dark" ? lists_dark : lists_light}
+                            className="h-50 rounded-lg border-1 border-border"
+                        />
+                    </div>
+                </div>
+                <div className="w-full flex justify-end mb-8">
+                    <div className="flex border-border border-t-2 border-b-2 border-l-2 bg-surface text-content px-2 py-2 rounded-l-lg max-w-11/12 gap-8">
+                        <img
+                            src={theme === "dark" ? together_dark : together_light}
+                            className="w-1/3 rounded-lg border-1 border-border"
+                        />
+                        <p className="text-content text-2xl content-center">Use the 'Add to list' button when viewing a list to easily add all of the necessary ingredients to your lists<br />
+                            Or, use the 'From Recipe' button when viewing a list to select ingredients from any of your saved recipes</p>
+                    </div>
+                </div>
+                <div className="w-full flex justify-start mb-8">
+                    <div className="flex border-border border-t-2 border-b-2 border-r-2 bg-surface text-content px-2 py-2 rounded-r-lg max-w-11/12 gap-8">
+                        <p className="text-content text-2xl content-center">Create and manage custom ingredients, tags, and units from the profile page<br />
+                            These can also be created while adding to a recipe or list!</p>
+                        <img
+                            src={theme === "dark" ? profile_dark : profile_light}
+                            className="h-50 rounded-lg border-1 border-border"
+                        />
+                    </div>
+
+                </div>
+                {!user &&
+                    <button
+                        className="mb-8 font-semibold text-2xl py-2 px-4 rounded-md bg-primary hover:bg-primary-hv text-content cursor-pointer"
+                        onClick={() => navigate('/signup')}
+                    >
+                        Get Started
+                    </button>
+                }
+            </div>) : (
+            <div className="flex flex-col w-full items-center">
+                <img src={logo} className="h-24" />
+                <h1 className="text-5xl font-semibold text-primary mb-4">RecipeMan</h1>
+                <div className="flex border-border border-t-2 border-b-2 bg-surface text-content px-2 py-2 mb-8">
+                    <p className="text-content content-center px-1 py-1 text-center">Save and manage all your recipes and shopping lists in one place<br />
                         Seamlessly create shopping lists based on the ingredients you need for your recipes<br />
                         Organize your ingredients and recipes with customizable tags<br />
                         Create custom ingredients and measurement units to fit your needs</p>
                 </div>
-            </div>
-            <div className="w-full flex justify-end mb-8">
-                <div className="flex border-border border-t-2 border-b-2 border-l-2 bg-surface text-content px-2 py-2 rounded-l-lg max-w-11/12 gap-8">
+                <div className="flex flex-col border-border border-t-2 border-b-2 bg-surface text-content px-2 py-2 w-full gap-2 mb-8">
+                    <p className="flex flex-col text-content content-center text-center">Use the 'recipes' page to view, edit, and create new recipes<br />
+                        Search and filter recipes by tags to find exactly what you're looking for<br />
+                        {"Tip: You can use {{double brackets}} around a number in the instructions of your recipe to enable scaling"}</p>
                     <img
                         src={theme === "dark" ? recipes_dark : recipes_light}
-                        className="max-w-full max-h-full h-75 rounded-lg border-1 border-border"
+                        className="rounded-lg border-1 border-border"
                     />
-                    <p className="text-content text-xl content-center">[recipes info] Use the 'recipes' page to view, edit, and create new recipes<br />
-                        Search and filter recipes by tags to find exactly what you're looking for<br />
-                        Scale recipes to adjust ingredient quantities based on your needs<br />
-                        {"Tip: You can use {{double brackets}} around a number in the instructions of your recipe to enable scaling"}</p>
-
                 </div>
-            </div>
-            <div className="w-full flex justify-start mb-8">
-                <div className="flex border-border border-t-2 border-b-2 border-r-2 bg-surface text-content px-2 py-2 rounded-r-lg max-w-11/12 gap-8">
-                    <p className="text-content text-xl content-center">[Lists info] Use the 'lists' page to make and manage your shopping lists<br />
+
+                <div className="flex flex-col border-border border-t-2 border-b-2 bg-surface text-content px-2 py-2 w-full gap-2 mb-8">
+                    <p className="text-content  content-center text-center">Use the 'lists' page to make and manage your shopping lists<br />
                         List items can be sorted by tags for easy organization<br />
-                        Multiple instances of the same item with compatible units will be automatically converted</p>
+                        Multiple instances of the same item with compatible units will be automatically combined</p>
                     <img
                         src={theme === "dark" ? lists_dark : lists_light}
-                        className="h-50 rounded-lg border-1 border-border"
+                        className="rounded-lg border-1 border-border"
                     />
                 </div>
-            </div>
-            <div className="w-full flex justify-end mb-8">
-                <div className="flex border-border border-t-2 border-b-2 border-l-2 bg-surface text-content px-2 py-2 rounded-l-lg max-w-11/12 gap-8">
+                <div className="flex flex-col border-border border-t-2 border-b-2 bg-surface text-content px-2 py-2 w-full gap-2 mb-8">
+
+                    <p className="text-content content-center text-center">Use the 'Add to list' button when viewing a list to easily add all of the necessary ingredients to your lists<br />
+                        Or, use the 'From Recipe' button when viewing a list to select ingredients from any of your saved recipes</p>
                     <img
                         src={theme === "dark" ? together_dark : together_light}
-                        className="h-50 rounded-lg border-1 border-border"
+                        className=" rounded-lg border-1 border-border"
                     />
-                    <p className="text-content text-xl content-center">[Bringing it together] Use the 'Add to list' button when viewing a list to easily add all of the necessary ingredients to your lists<br />
-                        Or, use the 'From Recipe' button when viewing a list to select ingredients from any of your saved recipes</p>
                 </div>
-            </div>
-            <div className="w-full flex justify-start mb-8">
-                <div className="flex border-border border-t-2 border-b-2 border-r-2 bg-surface text-content px-2 py-2 rounded-r-lg max-w-11/12 gap-8">
-                    <p className="text-content text-xl content-center">Create and manage custom ingredients, tags, and units from the profile page<br />
+
+
+                <div className="flex flex-col border-border border-t-2 border-b-2 bg-surface text-content px-2 py-2 w-full gap-2 mb-8">
+                    <p className="text-content content-center text-center">Create and manage custom ingredients, tags, and units from the profile page<br />
                         These can also be created while adding to a recipe or list!</p>
                     <img
                         src={theme === "dark" ? profile_dark : profile_light}
-                        className="h-50 rounded-lg border-1 border-border"
+                        className="rounded-lg border-1 border-border"
                     />
                 </div>
-
+                {!user &&
+                    <button
+                        className="mb-8 font-semibold text-2xl py-2 px-4 rounded-md bg-primary hover:bg-primary-hv text-content cursor-pointer"
+                        onClick={() => navigate('/signup')}
+                    >
+                        Get Started
+                    </button>
+                }
             </div>
-            {!user &&
-                <button
-                    className="mb-16 font-semibold text-2xl py-2 px-4 rounded-md bg-primary hover:bg-primary-hv text-content cursor-pointer"
-                    onClick={() => navigate('/signup')}
-                >
-                    Get Started
-                </button>
-            }
-        </div>
+        )
     );
 };
 
