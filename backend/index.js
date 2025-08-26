953eda919269b4ac45f06b432d7ca1029675cc2f
JPR3 <jpr3@riganati.com>
2025-08-26 16:36:54 -0400

    Fixed issue with wildcard path selection

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use('/api', tagsRoutes)
 
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-app.get('/{*any}', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
